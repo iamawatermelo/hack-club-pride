f33c406859556066fcec6139ab2bbb59a4e72651
Sarah C <144068104+iamawatermelo@users.noreply.github.com>
2025-01-27 21:30:56 +0000

    cba

--- scripts/pride.js
+++ scripts/pride.js
@@ -1,15 +1,15 @@
 console.log("[hack-club-pride] Listening for changes")
 
-const try_replace = () => {
-  const el = document.getElementsByClassName("p-account_switcher__team_icon--current")[0];
-  if (el === undefined) {
-    console.log("[hack-club-pride] Trying again in 200ms...");
-    setTimeout(try_replace, 200);
-    return;
+let el;
+
+const tryReplace = () => {
+  el = document.getElementsByClassName("p-account_switcher__team_icon--current")[0];
+  if (el) {
+    el.style.backgroundImage = `url("https://assets.hackclub.com/icon-progress-square.png")`;
   }
   
-  console.log("[hack-club-pride] Replacing background image");
-  el.style.backgroundImage = `url("https://assets.hackclub.com/icon-progress-square.png")`;
+  // literally cba doing this correctly
+  setTimeout(tryReplace, 200);
 };
 
-try_replace();
+tryReplace();
