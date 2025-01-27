7560aa6da75b11e9661ddc62f3929baa1ed18e5c
Sarah C <144068104+iamawatermelo@users.noreply.github.com>
2025-01-27 22:25:31 +0000

    why am i even using js for this

--- scripts/pride.js
+++ scripts/pride.js
@@ -1,15 +1,6 @@
-console.log("[hack-club-pride] Listening for changes")
-
-let el;
-
-const tryReplace = () => {
-  el = document.getElementsByClassName("p-account_switcher__team_icon--current")[0];
-  if (el) {
-    el.style.backgroundImage = `url("https://assets.hackclub.com/icon-progress-square.png")`;
+document.head.innerHTML += `<style>
+  /* Inserted by hack-club-pride */
+  .p-account_switcher__team_icon--current {
+    background-image: url("https://assets.hackclub.com/icon-progress-square.png") !important;
   }
-  
-  // literally cba doing this correctly
-  setTimeout(tryReplace, 200);
-};
-
-tryReplace();
+</style>`;
